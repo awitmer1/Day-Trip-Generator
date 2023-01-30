a5b459aaf6d4a37f2a027d82513ae227fd5fd561
Andrew Witmer <witmer.a@gmail.com>
2023-01-30 14:45:12 -0800

    Added main.py, project user story, function structures

---
+++ main.py
@@ -0,0 +1,50 @@
+"""
+(5 points): As a developer, I want to make at least three commits with descriptive messages 
+
+(5 points):  As a developer, I want to store my destinations, restaurants, mode of transportation, 
+and entertainment in their own separate Lists. (String Values)(at least 4)
+
+(5 points): As a user, I want a destination to be randomly selected for my day trip. 
+
+(5 points): As a user, I want a restaurant to be randomly selected for my day trip
+
+(5 points): As a user, I want a mode of transportation to be randomly selected for my day trip. 
+
+(5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+
+(15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, 
+and/or form of entertainment if I dont like one or more of those things.
+
+(10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections.
+
+(10  points): As a user, I want to display my completed trip in the console
+
+(5 points): Single Responsibility: As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing! 
+"""
+
+import random
+
+def run_day_trip_generator():
+
+def print_full_trip(list_of_options):
+
+def generate_random_item(list_of_items):
+
+def determine_satisfaction(current_trip, trip_options):
+
+def re_select(current_trip, options):
+
+run_day_trip_generator()
+
+
+
+
+
+
+destination_list = []
+
+restaurant_list = []
+
+transportation_mode = []
+
+entertainment_list = []
